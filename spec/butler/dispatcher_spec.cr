9b4c1ccb6a1ece3ea651a351e3d5ff0fcbb76fc7
Owajigbanam Ogbuluijah <xigbanam@gmail.com>
2020-06-06 09:11:49 +0100

    Put tests in modules; don't litter the global scope

--- spec/butler/dispatcher_spec.cr
+++ spec/butler/dispatcher_spec.cr
@@ -1,9 +1,17 @@
 require "../spec_helper"
 
-describe Butler::Dispatcher do
-  it "rejects unsupported commands" do
-    expect_raises(Butler::UnknownInstruction) do
-      Butler::Dispatcher.dispatch!(["igbanam"])
+module Butler
+  describe Dispatcher do
+    it "rejects unsupported instructions" do
+      expect_raises(UnknownInstruction) do
+        Dispatcher.dispatch!(["igbanam"])
+      end
+    end
+
+    it "routes 'task add' to the AddTask instruction" do
+      intent = "task add Begin!".split
+      instruction = Dispatcher.new(intent.first, intent[1..-1]).route
+      instruction.is_a?(Instruction::CreateTask).should be_true
     end
   end
 end
